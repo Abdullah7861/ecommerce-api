04074a50cc7a8d0a7c02e084afe05481c1b9e534
Muhammad Abdullah <m.a.abdullahch@gmail.com>
2025-05-18 19:27:28 +0500

    Indexes on product_srno

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import datetime
 import uuid
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Index, SQLModel
 from uuid import UUID
 
 class Products(SQLModel, table=True):
@@ -12,10 +12,14 @@ class Products(SQLModel, table=True):
     price: int 
     category: str
 
+    __table_args__ = (Index("idx_products_product_srno", "product_srno"),)
+
 class Inventory(SQLModel, table=True):
     product_srno:int  = Field(foreign_key="products.product_srno", primary_key=True)
     amount_available:int
     last_updated_date:datetime.date = Field(default_factory=datetime.datetime.now(datetime.UTC).date, nullable=False)
+
+    __table_args__ = (Index("idx_inventory_product_srno", "product_srno"),)
 
 
 class Inventory_history(SQLModel, table=True):
@@ -31,4 +35,4 @@ class Sales(SQLModel, table=True):
     no_of_items:int
     order_date: datetime.date = Field(default_factory=datetime.datetime.now(datetime.UTC).date, nullable=False)
 
-
+    __table_args__ = (Index("idx_sales_product_srno", "product_srno"),)
